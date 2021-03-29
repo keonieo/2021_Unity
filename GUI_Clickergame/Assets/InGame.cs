a7f7135c2c3f9cc7d0b26d39a72c5844f7c3e6da
keonieo <44870231+keonieo@users.noreply.github.com>
2021-03-29 21:52:37 +0900

    Unity

---
+++ GUI_Clickergame/Assets/InGame.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    int myNum;
+    public int GiftNum;
+    public GameObject Box;
+    public GameObject Gift;
+    public Text numTxt;
+    public GameObject B_Reset;
+
+    void Start()
+    {
+        numTxt.text = "0";
+
+        GiftNum = 10;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+     public void Click()
+        {
+            myNum++;
+            numTxt.text = myNum.ToString ();
+            Debug.Log("myNum : " + myNum);
+            if(myNum == GiftNum) {
+                Gift.SetActive(true);
+                Box.SetActive(false);
+                B_Reset.SetActive(true);
+            }
+        }
+
+    public void Onclick()
+    {
+        Gift.SetActive(false);
+                Box.SetActive(true);
+                B_Reset.SetActive(false);
+
+        myNum = 0;
+        numTxt.text = myNum.ToString ();
+    }
+}
